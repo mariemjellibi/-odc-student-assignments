e32d57199c85bb3e3c61f6b68b387dd122a0e3b6
mariemjellibi <mariemjellibi4@gmail.com>
2025-02-10 21:51:57 +0100

    my contribuition

--- client/types.d.ts
+++
@@ -1,8 +0,0 @@
-import {Key} from "react"
-export type Phrase = {
-  _id: Key;
-  translation: String;
-  japanese?: { romanization: String; native: String };
-  korean?: { romanization: String; native: String };
-  turkish?: { romanization: String; native: String };
-};
